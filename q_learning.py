0a5c835f7346830efd4be29c5f6c55d12e1c328e
Augustus <62512821+AugustusUkiyo@users.noreply.github.com>
2021-02-03 19:57:48 +0100

    Add files via upload

---
+++ q_learning.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 17:36:25 2020
+
+@author: ukiyo
+"""
+
+# Artificiql Intelligence for Business
+# Optimizing Warehouse Flows with Q-learning
+
+# Importing the libraries
+import numpy as np
+
+# Setting the parameters gamma and alpha for the Q-learning
+gamma = 0.75
+alpha = 0.9
+
+# PART 1 - DEFINING THE ENVIRONMENT
+
+# Defining the states
+location_to_state = {'A': 0,
+                     'B': 1,
+                     'C': 2,
+                     'D': 3,
+                     'E': 4,
+                     'F': 5,
+                     'G': 6,
+                     'H': 7,
+                     'I': 8,
+                     'J': 9,
+                     'K': 10,
+                     'L': 11}
+
+# Defining the actions
+actions = [x for x in range(0,12)]
+
+# Defining the rewards
+R = np.array([[0,1,0,0,0,0,0,0,0,0,0,0],
+              [1,0,1,0,0,1,0,0,0,0,0,0],
+              [0,1,0,0,0,0,1,0,0,0,0,0],
+              [0,0,0,0,0,0,0,1,0,0,0,0],
+              [0,0,0,0,0,0,0,0,1,0,0,0],
+              [0,1,0,0,0,0,0,0,0,1,0,0],
+              [0,0,1,0,0,0,1,1,0,0,0,0],
+              [0,0,0,1,0,0,1,0,0,0,0,1],
+              [0,0,0,0,1,0,0,0,0,1,0,0],
+              [0,0,0,0,0,1,0,0,1,0,1,0],
+              [0,0,0,0,0,0,0,0,0,1,0,1],
+              [0,0,0,0,0,0,0,1,0,0,1,0]])
+
+# PART 2 - BUILDING THE AI SOLUTION WITH Q-LEARNING
+        
+
+# PART 3 - GOING INTO PRODUCTION
+
+# Making a mapping from the states to the locations
+state_to_location = {state: location for location, state in location_to_state.items()}
+
+# Making the final function that will return the optimal route
+def route(starting_location, ending_location):
+    R_new = np.copy(R)
+    ending_state = location_to_state[ending_location]
+    R_new[ending_state, ending_state] = 1000
+    # Initializing the Q-Values
+    Q = np.array(np.zeros([12,12]))
+
+    # Implementing the Q-Learning
+    for i in range(1000):
+        current_state = np.random.randint(0,12)
+        playable_actions = []
+        for j in range(12):
+            if R_new[current_state, j] > 0:
+                playable_actions.append(j)
+        next_state = np.random.choice(playable_actions)
+        TD = R_new[current_state, next_state] + gamma * Q[next_state, np.argmax(Q[next_state,])] - Q[current_state, next_state]
+        Q[current_state, next_state] = Q[current_state, next_state] + alpha * TD
+    route = [starting_location]
+    next_location = starting_location
+    while (next_location != ending_location):
+        starting_state = location_to_state[starting_location]
+        next_state = np.argmax(Q[starting_state,])
+        next_location = state_to_location[next_state]
+        route.append(next_location)
+        starting_location = next_location
+    return route
+
+# PART 3 - GOING INTO PRODUCTION
+
+# Printing the final route
+print('Route:')
+print(route('E', 'G'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
